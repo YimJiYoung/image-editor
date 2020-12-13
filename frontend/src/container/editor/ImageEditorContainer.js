b25a1e61e2afec794c58709186082dc397cc4422
YimJiYoung <chiy323@gmail.com>
2020-12-13 11:43:58 +0900

    Feat: theme 테마 수정

--- frontend/src/container/editor/ImageEditorContainer.js
+++ frontend/src/container/editor/ImageEditorContainer.js
@@ -1,39 +1,52 @@
-import React, { useContext, useEffect } from 'react';
-import 'tui-image-editor/dist/tui-image-editor.css'
-import ImageEditor from '@toast-ui/react-image-editor'
-import userContext from '../../utils/context';
+import React, { useContext, useEffect } from "react";
+import "tui-image-editor/dist/tui-image-editor.css";
+import ImageEditor from "@toast-ui/react-image-editor";
+import userContext from "../../utils/context";
+import whitetheme from "../../utils/theme";
 
 const ImageEditorContainer = () => {
-    const editorRef = React.createRef();
-    const { data, setData } = useContext(userContext);
-    const { editingImageUrl } = data;
-    const sampleImageUrl = 'https://kr.object.ncloudstorage.com/image-editor/mountain_%2Bwallpaper.jpg';
+  const editorRef = React.createRef();
+  const { data, setData } = useContext(userContext);
+  const { editingImageUrl } = data;
+  const sampleImageUrl =
+    "https://kr.object.ncloudstorage.com/image-editor/sample.jpg";
 
-    return (
+  return (
     <ImageEditor
-        ref={editorRef}
-        includeUI={{
+      ref={editorRef}
+      includeUI={{
         loadImage: {
-          path: editingImageUrl? editingImageUrl: sampleImageUrl,
-          name: 'image'
+          path: editingImageUrl ? editingImageUrl : sampleImageUrl,
+          name: "image",
         },
-        // theme: 'whitetheme',
-        menu: ['crop', 'flip', 'rotate', 'draw', 'shape', 'icon', 'text', 'mask', 'filter'],
-        initMenu: 'filter',
+        theme: whitetheme,
+        menu: [
+          "crop",
+          "flip",
+          "rotate",
+          "draw",
+          "shape",
+          "icon",
+          "text",
+          "mask",
+          "filter",
+        ],
+        initMenu: "filter",
         uiSize: {
-            width: '100%',
-            height: '700px'
+          width: "100%",
+          height: "750px",
         },
-        menuBarPosition: 'bottom'
-        }}
-        cssMaxHeight={500}
-        cssMaxWidth={700}
-        selectionStyle={{
+        menuBarPosition: "bottom",
+      }}
+      cssMaxHeight={500}
+      cssMaxWidth={700}
+      selectionStyle={{
         cornerSize: 20,
-        rotatingPointOffset: 70
-        }}
-        usageStatistics={true}
-    />)      
-  };
+        rotatingPointOffset: 70,
+      }}
+      usageStatistics={true}
+    />
+  );
+};
 
 export default ImageEditorContainer;
